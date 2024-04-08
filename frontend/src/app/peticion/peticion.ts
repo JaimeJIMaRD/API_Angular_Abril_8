497c573a6867b456d6fb12472656ce1daa7a46ae
daw203 <dam05.2023@gmail.com>
2024-04-08 13:52:11 +0200

    first commit

---
+++ frontend/src/app/peticion/peticion.ts
@@ -0,0 +1,13 @@
+export interface Peticion {
+    categoria: any;
+    id: number;
+    titulo: string;
+    descripcion: string;
+    destinatario: string;
+    firmantes: number;
+    estado: string;
+    user_id: number;
+    categoria_id: number;
+    file:string;
+    files:any;
+}
